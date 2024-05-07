48c2b011c61e63c466c7e500f6e040625870ed0b
TimoSHka121 <tumofii2@gmail.com>
2024-05-07 17:12:09 +0300

    Update Host.cpp

--- src/Host.cpp
+++ src/Host.cpp
@@ -47,16 +47,24 @@ void Host::askName()
     }
     name = input;
 }
-bool Host::askIfPhraseFromFile()
-{
+int askIfPhraseFromFile() {
     int gameMode;
-    do
-    {
-        std::cout << "Хто буде загадувати слово: Хост (введіть 1) або комп'ютер (введіть 2): ";
+    do {
+        std::cout << "Who will provide the word: Host (enter 1) or computer (enter 2): ";
         std::cin >> gameMode;
+        if (std::cin.fail()) { 
+            std::cin.clear(); 
+            std::cin.ignore(10000, '\n'); 
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+            continue; 
+        }
+        if (gameMode != 1 && gameMode != 2) { 
+            std::cout << "Invalid input. Please enter 1 or 2." << std::endl;
+        }
     } while (gameMode != 1 && gameMode != 2);
-    return (gameMode - 1);
+    return gameMode;
 }
+
 void Host::askPhrase(Phrase &phrase)
 {
     std::string temp;
@@ -66,28 +74,46 @@ void Host::askPhrase(Phrase &phrase)
         std::getline(std::cin, temp);
     } while(!phrase.setPhrase(temp));
 }
-int Host::askNumberOfPlayers() 
-{
-    int numberOfPlayer;
-    do
-    {
-        std::cout << "Введіть кількість гравців (від 1 до 4): ";
-        std::cin >> numberOfPlayer;
-    } while (numberOfPlayer < 1 || numberOfPlayer > 4);
-    return numberOfPlayer;
+
+int askNumberOfPlayers() {
+    int numberOfPlayers;
+    do {
+        std::cout << "Please input the number of players (1 to 4): ";
+        std::cin >> numberOfPlayers;
+        if (std::cin.fail()) { 
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+            continue;
+        }
+        if (numberOfPlayers < 1 || numberOfPlayers > 4) { 
+            std::cout << "Invalid input. Please enter a number between 1 and 4." << std::endl;
+        }
+    } while (numberOfPlayers < 1 || numberOfPlayers > 4);
+    return numberOfPlayers;
 }
-bool Host::askRestart()
-{
+
+bool askRestart() {
     bool restartGame;
     char choice;
-    do
-    {
-        std::cout << "Чи бажаєте ви завершити гру? (y/n): ";
+    do {
+        std::cout << "Do you want to restart the game? (y/n): ";
         std::cin >> choice;
+        if (std::cin.fail()) { 
+            std::cin.clear(); 
+            std::cin.ignore(10000, '\n'); 
+            std::cout << "Invalid input. Please enter a character." << std::endl;
+            continue; 
+        }
+        if (choice != 'y' && choice != 'n') { 
+            std::cout << "Invalid input. Please enter 'y' or 'n'." << std::endl;
+        }
+        std::cin.ignore(10000, '\n'); 
     } while (choice != 'y' && choice != 'n');
-    restartGame = (choice == 'n') ? true : false;
+    restartGame = (choice == 'n') ? true : false; 
     return restartGame;
 }
+
 std::string Host::getName() const
 {
     return name;
