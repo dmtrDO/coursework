0b0ca4e6be75d2686fca1cc8ce7994305fce81f8
dmtrDO <dmytro.domaratskiy@gmail.com>
2024-05-07 16:44:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include "asd.h"
-using namespace std;
-
-
 
 int main()
 {
-    A obj;
-    obj.x = 5;
-    
-    cout << "Hello World!" << endl;
-    cout << "Наша курсова буде легенарна" << endl;
+    Game game;
+    game.start();
 }
